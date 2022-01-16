0380b3bd0888e7310a50be9fd89db9286ac1b294
Tony LaForgia <alforgia428@gmail.com>
2022-01-15 18:54:03 -0600

    Added object + function test

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,4 +1,15 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection (title, artist, yearPublished)
+function addToCollection (title, artist, yearPublished){
+  let musicList = {
+    title: '',
+    artist: '',
+    yearPublished: '',
+  }
+  collection.push(musicList);
+  return musicList;
+}
+// Test
+console.log(addToCollection());
+console.log(collection);
