3a6155fc2e6adcea2321b8c71aa2e41bcdcf5be1
Tony LaForgia <alforgia428@gmail.com>
2022-01-16 14:56:19 -0600

    Added 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,20 +2,25 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection (title, artist, yearPublished){
-  let musicList = {
+  const musicList = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
   }
   collection.push(musicList);
+  console.log(collection);
+  // console.log(`${title} by the ${artist} was released in ${yearPublished}`);
   return musicList;
 }
  // // Test
 // console.log(addToCollection());
 // console.log(collection);
 
-addToCollection('DARE', 'Gorillaz', '2005');
-addToCollection('Enter the Wu-Tang (36 Chambers)', 'Wu-Tang Clan', '1993');
+addToCollection('Demon Days', 'Gorillaz', '2005');
+addToCollection('Enter the Wu-Tang (36 Chambers', 'Wu-Tang Clan', '1993');
+addToCollection('Imperial Bedroom', 'Chet Baker', '1982');
+addToCollection('Power in Numbers', 'Jurassic 5', '2002');
+addToCollection('Stankonia', 'Outkast', '2000');
+addToCollection('Discovery', 'Face to Face', '2001');
 
-
-console.log(addToCollection(collection));
+// console.log(addToCollection(collection));
