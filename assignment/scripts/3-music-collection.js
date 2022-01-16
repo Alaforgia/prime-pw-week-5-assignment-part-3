32981e17e70314d209d59fe587132272f57ab2c4
Tony LaForgia <alforgia428@gmail.com>
2022-01-16 14:30:24 -0600

    Updated obj

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,19 +3,19 @@ let collection = [];
 
 function addToCollection (title, artist, yearPublished){
   let musicList = {
-    title: '',
-    artist: '',
-    yearPublished: '',
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
   }
   collection.push(musicList);
   return musicList;
 }
-// Test
-console.log(addToCollection());
-console.log(collection);
+ // // Test
+// console.log(addToCollection());
+// console.log(collection);
 
-collection.push('DARE', 'Gorillaz', '2005');
-collection.push('Enter the Wu-Tang (36 Chambers)', 'Wu-Tang Clan', '1993')
+addToCollection('DARE', 'Gorillaz', '2005');
+addToCollection('Enter the Wu-Tang (36 Chambers)', 'Wu-Tang Clan', '1993');
 
 
 console.log(addToCollection(collection));
